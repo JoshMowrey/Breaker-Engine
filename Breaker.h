dfda91c0a7a867a6a6e3d81f082ee1469ce11938
JoshMowrey <131113266+JoshMowrey@users.noreply.github.com>
2024-07-02 16:58:06 -0700

    Add files via upload

---
+++ Breaker.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Not for use within the Breaker Engine
+
+//includes from the breaker source code libraries via the src directory above current file
+#include "..\Breaker\BreakerApp.h"
+#include "..\Breaker\Core.h"
+
+
+//************ENTRY POINT************
+#include "..\Breaker\EntryPoint.h"
+//***********************************
